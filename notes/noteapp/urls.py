d1f5d6bf3bb27b90861c4c5796e0926107c7242b
Michail Kravchenko <66253910+kravchenmd@users.noreply.github.com>
2022-10-03 16:57:32 +0200

    Add `add tag`

--- notes/noteapp/urls.py
+++ notes/noteapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='main'),
+    path('tag/', views.tag, name='user_tag'),
 ]
--- notes/noteapp/views.py
+++ notes/noteapp/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .models import Tag
+
 
 # Create your views here.
+def main(request):
+    return render(request, 'noteapp/index.html', {})
+
+
+def tag(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        if name:
+            tl = Tag(name=name)
+            tl.save()
+        return redirect(to='main')
+    return render(request, 'noteapp/tag.html', {})
