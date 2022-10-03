c183196a2082c02a97a8822575bb3c1c9bb4d3ec
Michail Kravchenko <66253910+kravchenmd@users.noreply.github.com>
2022-10-03 17:37:57 +0200

    Add functionality for notes, add details of notes

--- notes/noteapp/urls.py
+++ notes/noteapp/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.main, name='main'),
     path('tag/', views.tag, name='user_tag'),
+    path('note/', views.note, name='note'),
+    path('detail/<int:note_id>', views.detail, name='detail'),
 ]
--- notes/noteapp/views.py
+++ notes/noteapp/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
 
-from .models import Tag
-from .forms import TagForm
+from .models import Tag, Note
+from .forms import TagForm, NoteForm
 
 
 # Create your views here.
 def main(request):
-    return render(request, 'noteapp/index.html', {})
+    notes = Note.objects.all()
+    return render(request, 'noteapp/index.html', {"notes": notes})
 
 
 def tag(request):
@@ -18,3 +19,26 @@ def tag(request):
         except ValueError as err:
             return render(request, 'noteapp/tag.html', {'form': TagForm(), 'error': err})
     return render(request, 'noteapp/tag.html', {'form': TagForm()})
+
+
+def detail(request, note_id):
+    note = Note.objects.get(pk=note_id)
+    # extend object from DB to have access to all tags when rendering
+    note.tag_list = ', '.join([str(name) for name in note.tags.all()])
+    return render(request, 'noteapp/detail.html', {"note": note})
+
+
+def note(request):
+    tags = Tag.objects.all()
+    if request.method == 'POST':
+        try:
+            list_tags = request.POST.getlist('tags')
+            form = NoteForm(request.POST)
+            new_note = form.save()
+            chosen_tags = Tag.objects.filter(name__in=list_tags)  # WHERE name in
+            for tag in chosen_tags.iterator():  # add m_to_m relationship
+                new_note.tags.add(tag)
+            return redirect(to='main')
+        except ValueError as err:
+            return render(request, 'noteapp/note.html', {"tags": tags, 'form': NoteForm(), 'error': err})
+    return render(request, 'noteapp/note.html', {"tags": tags, 'form': NoteForm()})
