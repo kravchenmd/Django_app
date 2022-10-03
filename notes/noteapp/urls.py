fddfde94574b4a03123b6ffa7355e13af9c09b7d
Michail Kravchenko <66253910+kravchenmd@users.noreply.github.com>
2022-10-03 18:40:21 +0200

    Add signup form and its functionality

--- notes/noteapp/urls.py
+++ notes/noteapp/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('detail/<int:note_id>', views.detail, name='detail'),
     path('done/<int:note_id>', views.set_done, name='set_done'),
     path('delete/<int:note_id>', views.delete_note, name='delete_note'),
+    path('signup/', views.signup_user, name='signup_user'),
+    path('login/', views.login_user, name='login_user'),
 ]
